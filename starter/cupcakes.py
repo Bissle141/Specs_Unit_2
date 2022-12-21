91d9449761dbec72ccaa6cb24c05fff886518855
Alyssa <alyssa.black141@gmail.com>
2022-12-21 13:50:30 -0700

    part 3 done

--- starter/cupcakes.py
+++ starter/cupcakes.py
@@ -1,9 +1,12 @@
 from abc import ABC, abstractmethod
+import csv
+from pprint import pprint
+
+cupcake_menu = 'sample.csv'
 
 class Cupcake(ABC):
-    size = 'regular'
-    
-    def __init__(self, name, price, flavor, frosting, filling):
+    def __init__(self, size, name, price, flavor, frosting, filling):
+        self.size = size
         self.name = name
         self.price = price
         self.flavor = flavor
@@ -41,7 +44,7 @@ class Large(Cupcake):
 class Regular(Cupcake):
     size = 'regular'
     
-    def __init__(self, name, price, flavor, frosting, filling):
+    def __init__(self, name, price, flavor, frosting, filling="none"):
         self.name = name
         self.price = price
         self.flavor = flavor
@@ -70,12 +73,48 @@ class Mini(Cupcake):
         
         
 my_cupcake_mini = Mini("Chocolate", 1.99, "Chocolate", "White")
-# print(my_cupcake_mini.name)
-# print(my_cupcake_mini.price)
-# print(my_cupcake_mini.size)
-# my_cupcake_mini.add_sprinkles('chocolate','rainbow')
-# print(my_cupcake_mini.sprinkles)
-# print(my_cupcake_mini.calculate_price(13))
 my_cupcake = Regular("Cookies and Cream", 2.99, "Chocolate", "Oreo", "Vanilla")
 my_cupcake.add_sprinkles('Oreo crumbs')
-print(my_cupcake.size,my_cupcake.name,my_cupcake.price,my_cupcake.flavor,my_cupcake.frosting,my_cupcake.filling, my_cupcake.sprinkles)
+
+
+cupcakes_list = [my_cupcake,my_cupcake_mini]
+
+
+def read_csv_file(csv_file):
+    with open(csv_file) as file:
+        lines = csv.DictReader(file)
+
+        for row in lines:
+            pprint(row)
+            
+def write_new_csv(file, cupcakes):
+    with open(file, "w", newline="\n") as csvfile:
+        fieldnames = ["size", "name", "price", "flavor", "frosting", "sprinkles", "filling"]
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        
+        writer.writeheader()
+        
+        for cupcake in cupcakes:
+            if hasattr(cupcake, "filling"):
+                writer.writerow({"size": cupcake.size, "name": cupcake.name, "price": cupcake.price, "flavor": cupcake.flavor, "frosting": cupcake.frosting, "sprinkles": cupcake.sprinkles, "filling": cupcake.filling})
+            
+            else:
+                writer.writerow({"size": cupcake.size, "name": cupcake.name, "price": cupcake.price, "flavor": cupcake.flavor, "frosting": cupcake.frosting, "sprinkles": cupcake.sprinkles})
+                
+def add_cupcake(file, cupcake):
+    cupcakes_list.append(cupcake)
+    with open(file, "a", newline="\n") as csvfile:
+        fieldnames = ["size", "name", "price", "flavor", "frosting", "sprinkles", "filling"]
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        if hasattr(cupcake, "filling"):
+            writer.writerow({"size": cupcake.size, "name": cupcake.name, "price": cupcake.price, "flavor": cupcake.flavor, "frosting": cupcake.frosting, "filling": cupcake.filling, "sprinkles": cupcake.sprinkles})
+        else:
+            writer.writerow({"size": cupcake.size, "name": cupcake.name, "price": cupcake.price, "flavor": cupcake.flavor, "frosting": cupcake.frosting, "sprinkles": cupcake.sprinkles})
+            
+            
+cupcake3 = Regular("choco", 2.99, "Chocolate", "fudge", "Vanilla")
+cupcake3.add_sprinkles('Oreo crumbs')
+# add_cupcake("cupcake_orders.csv", cupcake3)
+
+read_csv_file(cupcake_menu)
