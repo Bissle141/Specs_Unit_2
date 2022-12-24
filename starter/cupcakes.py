e606720a6c6c6f05b65092d7980a1e62e9a16aa8
Alyssa <alyssa.black141@gmail.com>
2022-12-23 18:45:28 -0700

    Donegit add .

--- starter/cupcakes.py
+++ starter/cupcakes.py
@@ -72,49 +72,119 @@ class Mini(Cupcake):
         return quantity * self.price
         
         
-my_cupcake_mini = Mini("Chocolate", 1.99, "Chocolate", "White")
-my_cupcake = Regular("Cookies and Cream", 2.99, "Chocolate", "Oreo", "Vanilla")
-my_cupcake.add_sprinkles('Oreo crumbs')
+# my_cupcake_mini = Mini("Chocolate", 1.99, "Chocolate", "White")
+# my_cupcake = Regular("Cookies and Cream", 2.99, "Chocolate", "Oreo", "Vanilla")
+# my_cupcake.add_sprinkles('Oreo crumbs')
 
 
-cupcakes_list = [my_cupcake,my_cupcake_mini]
+# cupcakes_list = [my_cupcake,my_cupcake_mini]
 
 
-def read_csv_file(csv_file):
-    with open(csv_file) as file:
-        lines = csv.DictReader(file)
+# def read_csv_file(csv_file):
+#     with open(csv_file) as file:
+#         lines = csv.DictReader(file)
 
-        for row in lines:
-            pprint(row)
+#         for row in lines:
+#             pprint(row)
             
-def write_new_csv(file, cupcakes):
-    with open(file, "w", newline="\n") as csvfile:
-        fieldnames = ["size", "name", "price", "flavor", "frosting", "sprinkles", "filling"]
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+# def write_new_csv(file, cupcakes):
+#     with open(file, "w", newline="\n") as csvfile:
+#         fieldnames = ["size", "name", "price", "flavor", "frosting", "sprinkles", "filling"]
+#         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        
+#         writer.writeheader()
+        
+#         for cupcake in cupcakes:
+#             if hasattr(cupcake, "filling"):
+#                 writer.writerow({"size": cupcake.size, "name": cupcake.name, "price": cupcake.price, "flavor": cupcake.flavor, "frosting": cupcake.frosting, "sprinkles": cupcake.sprinkles, "filling": cupcake.filling})
+            
+#             else:
+#                 writer.writerow({"size": cupcake.size, "name": cupcake.name, "price": cupcake.price, "flavor": cupcake.flavor, "frosting": cupcake.frosting, "sprinkles": cupcake.sprinkles})
+                
+# def add_cupcake(file, cupcake):
+#     with open(file, "a", newline="\n") as csvfile:
+#         fieldnames = ["size", "name", "price", "flavor", "frosting", "sprinkles", "filling"]
+#         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+#         if hasattr(cupcake, "filling"):
+#             writer.writerow({"size": cupcake.size, "name": cupcake.name, "price": cupcake.price, "flavor": cupcake.flavor, "frosting": cupcake.frosting, "filling": cupcake.filling, "sprinkles": cupcake.sprinkles})
+#         else:
+#             writer.writerow({"size": cupcake.size, "name": cupcake.name, "price": cupcake.price, "flavor": cupcake.flavor, "frosting": cupcake.frosting, "sprinkles": cupcake.sprinkles})
+    
+def write_list_to_csv(file, list):
+    with open(file, "w", newline='\n') as order_file:
+        fieldnames = ["size", "name", "price", "flavor", "frosting", "sprinkles", "filling", "quantity"]
+        writer = csv.DictWriter(order_file, fieldnames=fieldnames)
         
         writer.writeheader()
         
-        for cupcake in cupcakes:
-            if hasattr(cupcake, "filling"):
-                writer.writerow({"size": cupcake.size, "name": cupcake.name, "price": cupcake.price, "flavor": cupcake.flavor, "frosting": cupcake.frosting, "sprinkles": cupcake.sprinkles, "filling": cupcake.filling})
-            
+        writer.writerows(list)  
+    
+def get_cupcakes(file):
+    with open(file) as cupcakefile:
+        reader = csv.DictReader(cupcakefile)
+        reader = list(reader)
+        return reader
+    
+def find_cupcake(file, name):
+    for cupcake in get_cupcakes(file):
+        if cupcake["name"] == name:
+            return cupcake
+    return None    
+
+def add_cupcake_dictionary(file, cupcake):
+    order_list = get_cupcakes(file)
+    name = cupcake['name']
+    exists = False
+    
+    for item in order_list:
+        if item['name'] == name:
+            item['quantity'] = 1 + int(item['quantity'])
+            exists = True
+            break
+    if not exists: 
+        cupcake['quantity'] = 1
+        order_list.append(cupcake)
+    
+    write_list_to_csv(file, order_list)     
+        
+def get_total(file):
+    total = 0.00
+    for cupcake in get_cupcakes(file):
+        total += (float(cupcake["price"]) *  float(cupcake["quantity"]))
+        
+    return round(total,2)
+
+def del_cupcake(file, name):
+    order_list = get_cupcakes(file)
+    
+    for item in order_list:
+        if item['name'] == name:
+            order_list.remove(item)
+            break
+    
+    write_list_to_csv(file, order_list)
+
+def add_to_quan(file, name):
+    order_list = get_cupcakes(file)
+    
+    for item in order_list:
+        if item['name'] == name:
+            item['quantity'] = 1 + int(item['quantity'])
+            break
+        
+    write_list_to_csv(file, order_list)
+
+def minus_from_quan(file, name):
+    order_list = get_cupcakes(file)
+    
+    for item in order_list:
+        if item['name'] == name:
+            if int(item['quantity']) == 1:
+                order_list.remove(item)
+                break
             else:
-                writer.writerow({"size": cupcake.size, "name": cupcake.name, "price": cupcake.price, "flavor": cupcake.flavor, "frosting": cupcake.frosting, "sprinkles": cupcake.sprinkles})
-                
-def add_cupcake(file, cupcake):
-    cupcakes_list.append(cupcake)
-    with open(file, "a", newline="\n") as csvfile:
-        fieldnames = ["size", "name", "price", "flavor", "frosting", "sprinkles", "filling"]
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
-        if hasattr(cupcake, "filling"):
-            writer.writerow({"size": cupcake.size, "name": cupcake.name, "price": cupcake.price, "flavor": cupcake.flavor, "frosting": cupcake.frosting, "filling": cupcake.filling, "sprinkles": cupcake.sprinkles})
-        else:
-            writer.writerow({"size": cupcake.size, "name": cupcake.name, "price": cupcake.price, "flavor": cupcake.flavor, "frosting": cupcake.frosting, "sprinkles": cupcake.sprinkles})
-            
-            
-cupcake3 = Regular("choco", 2.99, "Chocolate", "fudge", "Vanilla")
-cupcake3.add_sprinkles('Oreo crumbs')
-# add_cupcake("cupcake_orders.csv", cupcake3)
-
-read_csv_file(cupcake_menu)
+                item['quantity'] = int(item['quantity']) - 1
+                break
+    
+    write_list_to_csv(file, order_list)
--- starter/server.py
+++ starter/server.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template
+import pprint
+from flask import Flask, render_template, url_for, redirect
+
+from cupcakes import get_cupcakes, find_cupcake, add_cupcake_dictionary, get_total, del_cupcake, minus_from_quan, add_to_quan
 
 app = Flask(__name__)
 
@@ -9,15 +12,51 @@ def home():
 
 @app.route("/menu")
 def menu():
-    return render_template("menu.html")
+    return render_template("menu.html", cupcakes = get_cupcakes("CSVs\menu.csv"))
 
-@app.route("/menu-{cupcake}")
-def menu_single():
-    return render_template("menu_single.html")
+@app.route("/menu/<name>")
+def menu_single(name):
+    cupcake = find_cupcake("CSVs/menu.csv", name)
+    
+    if cupcake:
+        sprinkles = cupcake['sprinkles'].strip("[]''")
+        sprinkles = sprinkles.split("', '")
+        
+        return render_template("menu_single.html", cupcake=cupcake, sprinkles = sprinkles)
+    else:
+        return "Sorry cupcake not found."
+
+@app.route("/order_add/<name>")
+def add_cupcake(name):
+    cupcake = find_cupcake("CSVs/menu.csv", name)
+    
+    if cupcake: 
+        add_cupcake_dictionary("CSVs/order.csv", cupcake=cupcake)
+        return redirect(url_for("menu"))
+    else: 
+        return "Sorry cupcake not found."
+    
+@app.route("/order_del/<name>")
+def delete_cupcake(name):
+    del_cupcake("./CSVs/order.csv", name)
+    return redirect(url_for("order"))
+    
+@app.route("/add_quan/<name>")
+def add_quantity(name):
+    add_to_quan("CSVs/order.csv", name)
+    return redirect(url_for("order"))
+    
+@app.route("/minus_quan/<name>")
+def minus_quantity(name):
+    minus_from_quan("CSVs/order.csv", name)
+    return redirect(url_for("order"))
+    
 
 @app.route("/order")
 def order():
-    return render_template("order.html")
+    return render_template("order.html", order = get_cupcakes("CSVs/order.csv"), total = get_total("CSVs/order.csv"))
+
 
 if __name__ == "__main__":
     app.run(debug = True, port = 8000, host = "localhost")
+    
